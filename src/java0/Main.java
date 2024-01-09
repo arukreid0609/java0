77d81110985d88e67ffa98ea05bdd184328343c9
sasaki <y.sasaki.roppongi@gmail.com>
2024-01-09 21:20:30 +0900

    sasa

--- src/java0/Main.java
+++ src/java0/Main.java
@@ -4,7 +4,9 @@ public class Main{
 	public static void main(String[] args) {
 		// 自己紹介の文です
 		System.out.println("佐々木ですだよ");
+		System.out.println("佐々木ですです");
 		System.out.println("workブランチです");
+		System.out.println("eclipse");
 	}
 }
 
