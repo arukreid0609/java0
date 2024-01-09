faae906f258a20edc33c82ec3b77f12631d48d9b
sasaki <y.sasaki.roppongi@gmail.com>
2024-01-09 17:55:19 +0900

    自己紹介変更

--- src/java0/Main.java
+++ src/java0/Main.java
@@ -3,7 +3,7 @@ package java0;
 public class Main{
 	public static void main(String[] args) {
 		// 自己紹介の文です
-		System.out.println("佐々木です");
+		System.out.println("佐々木ですだよ");
 		System.out.println("workブランチです");
 	}
 }
